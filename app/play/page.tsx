bb7478363c5412550a301b8340b5be1f3c2051f5
MetodyPetrov <metody.p.petrov@gmail.com>
2024-08-26 01:10:15 +0300

    added Link routing

--- app/play/page.tsx
+++ app/play/page.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 export default function Game() {
     return (
-        <div className="w-full h-screen">
+        <>
             <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                <Link href="..." className="p-10 m-3 bg-gray-300 text-8xl font-bold transition-all duration-300 hover:shadow-xl hover:p-20">PROVIDER</Link>
-                <Link href="..." className="p-10 m-3 bg-green-300 text-8xl font-bold transition-all duration-300 hover:shadow-xl hover:p-20">GUESSER</Link>
+                <Link href="/play/provider" className="p-10 m-3 bg-gray-300 text-8xl font-bold transition-all duration-300 hover:shadow-xl hover:p-20">PROVIDER</Link>
+                <Link href="/play/guesser" className="p-10 m-3 bg-green-300 text-8xl font-bold transition-all duration-300 hover:shadow-xl hover:p-20">GUESSER</Link>
             </div>
-        </div>
+        </>
     )
 }
