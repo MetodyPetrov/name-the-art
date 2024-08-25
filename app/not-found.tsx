0009dd2f0e6b75cf8c69092198e2e9e2f06a6969
MetodyPetrov <metody.p.petrov@gmail.com>
2024-08-26 01:10:56 +0300

    added a not found page using the reserved file name

---
+++ app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+    return (
+        <div className="bg-black text-white w-full">
+            <h1 className="text-9xl font-semibold absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 whitespace-nowrap">Page Not Found!</h1>
+        </div>
+    )
+}
